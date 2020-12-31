fcdb0a792e2457ae29980cdf84855241e80c6968
DobbyUp <76684517+DobbyUp@users.noreply.github.com>
2020-12-31 15:38:15 -0300

    Update help.js

--- src/help.js
+++ src/help.js
@@ -1,65 +1,72 @@
 const help = (prefix) => {
 	return `> *Sticker Commands* <
-command : *${prefix}sticker* or *${prefix}stiker*
-desc : converter imagem / gif / vídeo em adesivo
-usage : responder imagem / gif / vídeo ou enviar imagem / gif / vídeo com legenda\n
-command : *${prefix}sticker nobg* or *${prefix}stiker nobg*
-desc : converter imagem em adesivo removendo o fundo
-usage : reply image, or send image with caption\n
-command : *${prefix}toimg*
-desc : converter adesivo em imagem
-usage : reply sticker\n
-command : *${prefix}tsticker* or *${prefix}tstiker*
-desc : converter texto em adesivo
-usage : *${prefix}tsticker text in here*\n
+
+
+           COMANDOS ONNI CHAN 2
+
+COMANDO: *${prefix}sticker* ou *${prefix}stiker*
+RESPOSTA : converter imagem / gif / vídeo em figurinha
+
+COMANDO: *${prefix}sticker nobg* ou *${prefix}stiker nobg*
+RESPOSTA : converter imagem em figurinha removendo o fundo
+
+COMANDO : *${prefix}toimg*:
+RESPOSTA : converter adesivo em imagem
+COMANDO : *${prefix}tsticker* ou *${prefix}tstiker*
+RESPOSTA : converter texto em adesivo
+
+
 > *Meme Commands* <
-command : *${prefix}meme*
-desc : imagens aleatórias de meme [english]
-usage : just send the command\n
-command : *${prefix}memeindo*
-desc : imagens aleatórias de meme [indo]
-usage : apenas envie o comando\n
-> *Others Commands* <
-command : *${prefix}gtts*
-desc : converter texto em fala / áudio
-usage : *${prefix}gtts [cc] [text]*\nexemplo : *${prefix}gtts ja On2-chan*\n
-command : *${prefix}loli*
-desc : imagens aleatórias de loli
-usage : apenas envie o comando\n
-command : *${prefix}nsfwloli*
-desc : imagens aleatórias de nsfw loli
-usage: basta enviar o comando\n
-command : *${prefix}url2img*
-desc : take web screenshots
-usage : *${prefix}url2img [tipe] [url]*\n
-command : *${prefix}simi*
+ COMANDO : *${prefix}meme*
+RESPOSTA : imagens aleatórias de meme [Português
+
+COMANDO: *${prefix}memeindo*
+RESPOSTA : imagens aleatórias de meme [indo]
+
+> *OUTROS CCOMANDOS* <
+COMANDO : *${prefix}gtts*
+RESPOSTA: converter texto em fala / áudio
+DESCEXEMPLO : *${prefix}gtts [cc] [text]*\nexemplo : *${prefix}gtts ja On2-chan*\n
+
+COMANDO : *${prefix}loli*
+RESPOSTA : imagens aleatórias de loli
+
+COMANDO : *${prefix}nsfwloli*
+RESPOSTA: imagens aleatórias de nsfw loli
+
+COMANDO : *${prefix}url2img*
+RESPOSTA : take web screenshots
+
+COMANDO : *${prefix}simi*
 desc : sua mensagem será respondida por simi
-usage : *${prefix}simi yourmessage*\n
+
 command : *${prefix}ocr*
 desc : pegue o texto na foto
-usage : responder imagem ou enviar imagem com legenda\n
-command : *${prefix}wait*
-desc : search anime with image [ What Anime Is This/That ]
-usage : responder imagem ou enviar imagem com legendan\n
-command : *${prefix}setprefix*
-desc : substituir prefixo
-usage : *${prefix}setprefix [text|optional]*\nexample : *${prefix}setprefix ?*
-note : This command can only be used by the bot owner\n
-> *Group Comands* <
-command : *${prefix}linkgroup*
-desc : pegue o link do grupo
-usage : just send the command\
-note : can only be used when the bot becomes admin, and the one who sends the command is admin!\n
-command : *${prefix}tagall*
-desc : marca todos os membros do grupo, incluindo administradores também
-usage : apenas envie o comando
-note : Este comando pode ser usado se você for um administrador do grupo\n
-command : *${prefix}simih*
-desc : ative o modo simi no grupo
-usage : *${prefix}simih 1* to activate simi mode and *${prefix}simih 0* para desativar o modo simi
-note : Este comando pode ser usado se você for um admin\n`
-}
 
-exports.help = help
+COMANDO : *${prefix}wait*
+RESPOSTA: pesquisar anime por imagem
 
+COMANDO : *${prefix}setprefix*
+RESPOSTA : substituir prefixo
+
+
+> *COMANDOS DE GRUPOS* <
+COMANDO: *${prefix}linkgroup*
+RESPOSTA: pegue o link do grupo
+
+COMANDO : *${prefix}tagall*
+RESPOSTA : Marca todos do grupo, incluindo adms
+
+COMANDO : *${prefix}listadmins*
+RESPOSTA :marca todos os admins do grupo
+
+COMANDO :*${prefix}kick @da pessoa*
+RESPOSTA :Ban em algum membro do grupo
+Obs: o bot precisa ser adm
+
+COMANDO :*${prefix}add numero da pessoa com dd(apenas numeros)
+RESPOSTA :Adicona alguem ao grupo
+Obs: O bot precisa ser adm
+
+exports.menu = menu
 
